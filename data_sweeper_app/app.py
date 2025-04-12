8308d141a868eb6adda11a96a93915d90e6ee7ad
AYeshsidd <159470548+AYeshsidd@users.noreply.github.com>
2025-04-12 14:31:35 +0500

    application using uv & streamlit

---
+++ data_sweeper_app/app.py
@@ -0,0 +1,152 @@
+import streamlit as st
+import pandas as pd
+import os
+from io import BytesIO
+
+st.set_page_config(page_title="Application Data sweeper", layout="wide")
+
+
+st.markdown("""
+<h1 style='color:yellow; font-family:Arial;'>Streamlit Data sweeper app 🗑️🛠️</h1>
+<h4 style='color:yellow; font-family:mono; font-weight:normal;'>Clean, filter, and download your data easily 🧹📊
+</h4>   """, unsafe_allow_html=True)
+
+# custom css
+st.markdown(
+    """
+            <style>.stApp{
+            background-color:black;
+            color:white;
+        
+            }
+            .h1{}
+            .features-heading {
+            color: white;
+            font-size: 40px;
+            margin-top:16px;
+            margin-bottom:10px;
+            
+            }
+
+            .features-text {
+            color: white; 
+            }
+
+             .stFileUploader label {
+            color:white;
+            font-size: 32px;  !important; 
+            font-weight:800; !important; 
+            }
+
+            .error{
+            color:white;
+            font:bold;
+            }
+
+            .cleaning{
+            color:white;
+            font-family: 'Montserrat', sans-serif;
+           font-size:32px;
+        
+            }
+            </style>
+            """,
+    unsafe_allow_html=True,
+)
+st.markdown(
+    """
+     <div class="features-heading">   
+   Features:
+ </div>
+
+ <div class = "features-text">    
+✅ Upload CSV or Excel files  </br>
+✅ Remove duplicates and missing values </br>
+✅ Filter and sort data dynamically </br>
+✅ Download the cleaned dataset instantly </div></br>""",
+    unsafe_allow_html=True,
+)
+
+upload_Files = st.file_uploader(
+    "Upload your CSV or EXCEL files here",
+    type=["csv", "xlsx"],
+    accept_multiple_files=True,
+)
+
+if upload_Files:
+    for file in upload_Files:
+        file_Extension = os.path.splitext(file.name)[-1]
+
+        if file_Extension == ".csv":
+            df = pd.read_csv(file)
+
+        elif file_Extension == ".xlsx":
+            df = pd.read_excel(file)
+
+        else:
+            st.markdown(
+                f"""<div class ="error"> you uploaded unsupported file {file_Extension }""",
+                unsafe_allow_html=True,
+            )
+            continue
+
+    st.write(f"File Name : **{file.name}**")
+    st.write(f"File Size : **{file.size}**")
+
+    st.write("Extracting the top records from the DataFrame")
+    st.dataframe(df.head())
+
+    st.markdown(
+        """<div class="cleaning">   
+   🎫Data cleanings here!"
+ </div>""",
+        unsafe_allow_html=True,
+    )
+
+    if st.checkbox(f"clean data for {file.name}"):
+        col1, col2 = st.columns(2)
+
+        with col1:
+
+         if st.button(f"Removing duplicates from {file.name}"):
+            df.drop_duplicates(inplace=True)
+            st.write("All duplicates removed")
+            st.success("✅ Duplicates removed!")
+            st.dataframe(df.head())
+
+
+        with col2:
+     
+         if st.button(f" missing values for {file.name}"):
+          numeric_cols = df.select_dtypes(include=["number"]).columns
+          df[numeric_cols] = df[numeric_cols].fillna(df[numeric_cols].mean())
+          st.success("✅ Missing values filled!")
+          st.dataframe(df.head())
+
+        st.write("missing values")
+
+        st.markdown("<h3 style='color:#1E90FF; font-family:Arial;'>📊 Select columns</h3>", unsafe_allow_html=True)
+
+        columns = st.multiselect(
+            f"choose columns for {file.name}", df.columns, default=df.columns
+        )
+
+        df = df[columns]
+
+        st.markdown("<h3 style='color:#1E90FF; font-family:Arial;'>📊 Data Visualization</h3>", unsafe_allow_html=True)
+
+        if st.checkbox(f"show visulaisation for {file.name}"):
+            st.bar_chart(df.select_dtypes(include="number").iloc)
+
+        
+        if st.checkbox(f"🛠️ Clean data for {file.name}"):
+            col1, col2 = st.columns(2)  
+
+            
+            # Handling Missing Values here
+            with col2:
+                if st.button(f"📌 Fill Missing Values for {file.name}"):
+                    numeric_cols = df.select_dtypes(include=['number']).columns
+                    df[numeric_cols] = df[numeric_cols].fillna(df[numeric_cols].mean())
+                    st.success("✅ Missing values filled!")
+                    st.dataframe(df.head())
